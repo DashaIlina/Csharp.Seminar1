d936c8e4c550117c915188ed62d0c18564cb8f49
Darya Ilina <ilinadarya.mow@bk.ru>
2022-03-10 08:54:09 +0300

    add files Task3's

---
+++ Tesk3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите номер дня недели ");
+
+int numday = Convert.ToInt32(Console.ReadLine());
+
+switch (numday)
+{
+    case 1:
+        Console.Write(" Понедельник ");
+        break;
+    case 2:
+        Console.Write(" Вторник ");
+        break;
+    case 3:
+        Console.Write(" Среда ");
+        break;
+    case 4:
+        Console.Write(" Четверг ");
+        break;
+    case 5:
+        Console.Write(" Пятница ");
+        break;
+    case 6:
+        Console.Write(" Суббота ");
+        break;
+    case 7:
+        Console.Write(" Воскресенье ");
+        break;
+    default: 
+        Console.Write(" Дня недели с таким номером нет ");
+        break;
+}
