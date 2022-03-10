e5c00a5830c8ffa155b2e2bfd02b1712edb38708
Darya Ilina <ilinadarya.mow@bk.ru>
2022-03-10 08:55:30 +0300

    add files Task4's

---
+++ Tesk4/Program.cs
@@ -0,0 +1,22 @@
+﻿public class task4 
+{
+public static void Main(string[] args) 
+{
+Console.WriteLine("Проверка числа на чётность.");
+Console.WriteLine("___________________________");
+Console.Write("Введите целое число: ");
+
+int num = Convert.ToInt32(Console.ReadLine()); 
+
+if (num % 2 == 0)
+{
+Console.Write("Число чётное. ");
+}
+else 
+{
+Console.Write("Число нечётное. ");
+}
+
+ 
+}
+}
