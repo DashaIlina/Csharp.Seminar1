1bc2d922cead1a398ba4831a303cff09ee7dd948
Darya Ilina <ilinadarya.mow@bk.ru>
2022-03-10 08:52:57 +0300

    add files Task1's

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите 2 числа: ");
+Console.Write("Ведите 1 число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите 2 число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2)
+{
+    Console.WriteLine(num1 + " является меньше " + num2 );
+}
+else if (num1 == num2)
+{
+    Console.WriteLine(num1 + " равно " + num2);
+}
+else 
+{
+    Console.WriteLine(num1 + " является больше " + num2);
+}
---
+++ Tesk1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+    Console.WriteLine("Введите 2 числа: ");
+    Console.Write("Ведите 1 число ");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Ведите 2 число ");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+
+    if (num1 == num2 * num2)
+    {
+        Console.WriteLine(num1 + " является квадратом " + num2);
+    }   
+    else 
+    {
+        Console.WriteLine(num1 + " не является квадратом " + num2);
+    }
+    
+
